56de67cefbfd43df71820d2c89223cb11b91e500
Richard Aldrich <rsie0001@student.monash.edu>
2019-08-21 08:47:13 +1000

    skeleton code

---
+++ waterwegenvic/waterwegenvic/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace waterwegenvic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Background()
+        {
+            ViewBag.Message = "Your background page.";
+
+            return View();
+        }
+
+        public ActionResult Substance()
+        {
+            ViewBag.Message = "Your substance page.";
+
+            return View();
+        }
+
+        public ActionResult Litter()
+        {
+            ViewBag.Message = "Your litter page.";
+
+            return View();
+        }
+
+        public ActionResult Groundwater()
+        {
+            ViewBag.Message = "Your groundwater page.";
+
+            return View();
+        }
+    }
+}
