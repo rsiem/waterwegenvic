80c2982a92e6dab8fd09e1d7b50d2954d232d365
Richard Aldrich <rsie0001@student.monash.edu>
2019-08-24 09:22:48 +1000

    add image and carousel into landing page

--- waterwegenvic/waterwegenvic/App_Start/BundleConfig.cs
+++ waterwegenvic/waterwegenvic/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace waterwegenvic
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/script").Include(
+                      "~/Scripts/site.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
