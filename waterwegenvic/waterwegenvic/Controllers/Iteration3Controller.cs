7386be816030569bbf619b73bb19e147482e29aa
Richard Aldrich <rsie0001@student.monash.edu>
2019-10-13 17:00:25 +1100

    iteration 3 working link

---
+++ waterwegenvic/waterwegenvic/Controllers/Iteration3Controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using waterwegenvic.Models;
+
+namespace waterwegenvic.Controllers
+{
+    public class Iteration3Controller : Controller
+    {
+        private gruModels db = new gruModels(WebConfigurationManager.ConnectionStrings["gruModels"].ConnectionString);
+
+        // GET: Iteration3
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Substance()
+        {
+            ViewBag.Message = "Your substance page.";
+
+            return View();
+        }
+
+        public ActionResult Litter()
+        {
+            ViewBag.Message = "Your litter page.";
+
+            return View();
+        }
+
+        public ActionResult Stormwater()
+        {
+            ViewBag.Message = "Your stormwater page.";
+
+            return View(db.grus.ToList());
+        }
+    }
+}
